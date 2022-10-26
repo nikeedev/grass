be6cda34dea6f958cfdc7b7b4b9baf6d1f6a1fbf
StioStudio <simon.s.johanssen@gmail.com>
2022-10-26 11:57:40 +0200

    updates

--- sandbox/MyGame.h
+++ sandbox/MyGame.h
@@ -7,7 +7,7 @@ class MyGame : public Application
 public:
 	
 
-	MyGame() : Application("My Game", vec2(800 , 600), Color(100, 149, 237), true) {};
+	MyGame() : Application("My Game", vec2(800, 600), Color(100, 149, 237), true) {};
 
 	void Once() override;
 	void Update(double ts) override;
